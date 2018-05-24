bafd017299bcf44bdff0d0ac58118f5d8358eefd
kylejan <kylejan.jan@gmail.com>
2018-05-25 02:15:10 +0800

    update

--- okex_client.py
+++ okex_client.py
@@ -19,18 +19,20 @@ if __name__ == '__main__':
     spot_api_functions = [func for func in dir(spot_api) if func[0] != '_']
     future_api_functions = [func for func in dir(future_api) if func[0] != '_']
 
-    parser = argparse.ArgumentParser(prog='python okex_client.py')
+    parser = argparse.ArgumentParser(prog='python okex_client.py', conflict_handler='resolve')
     sub_parser = parser.add_subparsers(help='api type help')
 
     spot_parser = sub_parser.add_parser('spot', help='spot api help')
     spot_parser.set_defaults(security_type='spot')
+    spot_op_group = spot_parser.add_mutually_exclusive_group()
     for func in spot_api_functions:
-        spot_parser.add_argument('--{}'.format(func))
+        spot_op_group.add_argument('--{}'.format(func))
 
-    # future_parser = sub_parser.add_parser('future', help='future api help')
-    # future_parser.set_defaults(security_type='future')
-    # for func in future_api_functions:
-    #     spot_parser.add_argument('--{}'.format(func))
+    future_parser = sub_parser.add_parser('future', help='future api help')
+    future_parser.set_defaults(security_type='future')
+    future_op_group = future_parser.add_mutually_exclusive_group()
+    for func in future_api_functions:
+        future_op_group.add_argument('--{}'.format(func))
 
     args = parser.parse_args()
     print(args.security_type)
